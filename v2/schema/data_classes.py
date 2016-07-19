b55d18fde5c21d0f3e30f88a7644da7dc83f826c
David Hassell <d.c.hassell@reading.ac.uk>
2016-07-19 13:59:18 +0100

    Up to date Simunlation

--- v2/schema/data_classes.py
+++ v2/schema/data_classes.py
@@ -82,50 +82,60 @@ def simulation():
         'base': 'activity.activity',
         'is_abstract': False,
         'properties': [
-            ('calendar', 'time.calendar', '0.1',
-                "The calendar used in the simulation."),
-            ('contact', 'str', '0.1',
-                "None"),
-            ('end_time', 'time.date_time', '0.1',
-                "The start date-time of the simulation. e.g. 2087-11-30 12:00:00."),
-            ('extra_attributes', 'shared.extra_attribute', '0.N',
-                "Additional attributes provided with simulation."),
-            ('forcing_index', 'int', '0.1',
-                "index for variant of forcing, e.g. 2."),
-            ('further_info_url', 'str', '0.1',
-                "On-line location of documentation."),
-            ('initialization_index', 'int', '0.1',
-                "Index variant of initialization method, e.g. 1."),
-            ('insitution', 'linked_to(shared.party)', '0.1',
-                "Institution which carried out the simulation."),
-            ('parent_simulation', 'activity.parent_simulation', '0.1',
-                "If appropriate, detailed information about how this simulation branched from a previous one."),
             ('part_of_project', 'linked_to(designing.project)', '1.N',
-                "Project or projects for which simulation was run."),
-            ('physics_index', 'int', '0.1',
-                "index for model physics, e.g. 3."),
-            ('primary_ensemble', 'linked_to(activity.ensemble)', '0.1',
-                "Primary Ensemble (ensemble for which this simulation was first run)."),
+                'Project or projects for which simulation was run'),
             ('ran_for_experiments', 'linked_to(designing.numerical_experiment)', '1.N',
-                "One or more experiments with which the simulation is associated."),
-            ('realization_index', 'int', '0.1',
-                "realization number, e.g. 5."),
-            ('references', 'str', '0.1',
-                "Published or web-based references that describe the data or methods used to produce it."),
-            ('start_time', 'time.date_time', '0.1',
-                "The start date-time of the simulation. e.g. 2012-04-01 00:00:00."),
-            ('sub_experiment', 'str', '0.1',
-                "For start-date ensembles, this will indicate the beginning year; for offline models driven by output from another model, this will provide the source_id and variant_label for the 'driving' model."),
+                'One or more experiments with which the simulation is associated'),
+            ('sub_experiment', 'designing.numerical_experiment', '0.1',
+                'For start-date ensembles, this will indicate the beginning year; for offline models driven by output from another model, this will provide the source_id and variant_label for the “driving” model.'),
             ('used', 'linked_to(science.model)', '1.1',
-                "The model used to run the simulation."),
+                'The model used to run the simulation'),
+            ('primary_ensemble', 'linked_to(activity.ensemble)', '0.1',
+                'Primary Ensemble (ensemble for which this simulation was first run).'),
+            ('insitution', 'str', 'shared_classes.party', '0.1',
+             'institution which carried out the simulation'),
+            ('parent_simulation', 'activity.parent_simulation', '0.1',
+                'If appropriate, detailed information about how this simulation branched from a previous one'),
+
+            # Time
+            ('start_time', 'time.date_time', '0.1',
+             'The start date-time of the simulation. e.g. 2012-04-01 00:00:00'),
+            ('end_time', 'time.date_time', '0.1',
+             'The start date-time of the simulation. e.g. 2087-11-30 12:00:00'),
+            ('calendar', 'time.calendar', '0.1',
+             'The calendar used in the simulation'),
+
+            # Ensemble member attributes
+            ('realization_index', 'int', '0.1',
+             'realization number, e.g. 5'),
+            ('initialization_index', 'int', '0.1',
+             'Index variant of initialization method, e.g. 1'),
+            ('physics_index', 'int', '0.1',
+             'index for model physics, e.g. 3'),
+            ('forcing_index', 'int', '0.1',
+             'index for variant of forcing, e.g. 2'),
             ('variant_info', 'str', '0.1',
-                "description of run variant differences, e.g. 'forcing: black carbon aerosol only'.")
-        ],
-        'constraints': [
-            ('cardinality', 'rationale', '0.0')
+             'description of run variant differences, e.g. "forcing: black carbon aerosol only"'),
+
+            # Contacts and references
+            ('contact', 'str', '0.1',
+             ''),
+            ('references', 'str', '0.1',
+             'Published or web-based references that describe the data or methods used to produce it'),
+
+            # Further Info URL
+            ('further_info_url', 'str', '0.1',
+             'On-line location of documentation'),
+
+            # Extra attributes
+            ('extra_attributes','shared.extra_attribute', '0.N',
+                'Additional attributes provided with simulation.')
+            ],
+
+        'constraints':[
+            ('cardinality', 'rationale', '0.0'),
         ]
     }
-
 
 def variable_collection():
     """A collection of variables within the scope of a code or process element.
