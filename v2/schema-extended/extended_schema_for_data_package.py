24dc24e25978e1a4abd7d9cdc123471dfd79e6a4
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-05-15 17:18:53 +0200

    Latest cim v2 generated artefacts

--- v2/schema-extended/extended_schema_for_data_package.py
+++ v2/schema-extended/extended_schema_for_data_package.py
@@ -144,7 +144,7 @@ modifications requirted to use it in the relevant component.
         'is_abstract': False,
         'is_document': False,
         'properties': [
-            ('modifications_applied', 'str', '1.1',
+            ('modifications_applied', 'str', '0.1',
                 "Describe modifications (if any) applied to the dataset prior to use. E.g. spatial interpolation, temporal averaging, etc."),
             ('original_data', 'data.dataset', '1.1',
                 "The source dataset, prior to any modifications"),
